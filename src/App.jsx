3bb699ceb43b17c863af52133e19dbeeac102c11
00iun <lsh681236@naver.com>
2025-02-18 11:26:29 +0900

    2일차

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>안녕하세요</p>
+      <p>오즈스쿨 수강 중인 이희정입니다</p>
+      <p>여긴 vite 예요</p>
+      <p>감사합니다</p>
+      <h1>2025.02.17</h1>
+      <p>AWS 코드 업데이트</p>
+    </>
+  )
+}
+
+export default App
